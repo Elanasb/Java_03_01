f7b7f2fd76391562eddee630a0ace3723f8dc0ee
Elanas <elaasinfo@gmail.com>
2018-01-31 10:24:10 +0200

    1 uzduotis

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Iveskite Skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 2 == 0) {
+            System.out.println("Skaicius yra lyginis");
+        } else {
+            System.out.println("Skaicius nelyginis");
+        }
+    }
+}
